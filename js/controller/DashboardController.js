8f3b91adba0722f8ffc424f08b545f7640a71284
Diego <di3g0d0ming05@gmail.com>
2017-09-30 20:07:12 -0300

    ajuste layout 2 e intregraçao com angular

--- js/controller/DashboardController.js
+++ js/controller/DashboardController.js
@@ -2,15 +2,34 @@
 
 angular.module('app').controller('DashboardController', DashboardController);
 
-DashboardController.$inject = ['DashboardService'];
+DashboardController.$inject = ['DashboardService', '$scope'];
 
-function DashboardController(DashboardService) {
+function DashboardController(DashboardService,$scope) {
+
     var vim = this;
-    vim.nome = {
-        'nome':'diego'
-    };   
 
-    alert(DashboardService.getDados("teste"));
- 
+    vim.mesAtual = DashboardService.getMesAtual();
+    vim.mesAnterior = DashboardService.getMesAnterior();
+    vim.Comparativo = DashboardService.getComparativo();
+
+    console.log(vim.mesAtual);
+    console.log(vim.mesAnterior);
+    console.log(vim.Comparativo);
+
+    $scope.colors = ['#45b7cd', '#ff6384', '#ff8e72'];
+    $scope.labels = vim.Comparativo.dias;
+    $scope.series = vim.Comparativo.mes;
+    $scope.data = vim.Comparativo.dados;
+
+
+    $scope.labels_mes_atual = vim.mesAtual.dias;
+    $scope.series_mes_atual = vim.mesAtual.mes;
+    $scope.data_mes_atual =  vim.mesAtual.dados;
+
+
+    $scope.labels_mes_anterior = vim.mesAnterior.dias;
+    $scope.series_mes_anterior = vim.mesAnterior.mes;
+    $scope.data_mes_anterior = vim.mesAnterior.dados;
+    
+
  }
- 
--- js/services/DashboardService.js
+++ js/services/DashboardService.js
@@ -8,11 +8,37 @@ function DashboardService() {
 
     var vim = this;
 
-    vim.getDados = function(teste){        
-        return teste;
+    vim.testeMesAtual = {
+      'mes' : 'Fevereiro',
+      'dias' : ['1','2','3','4','5','6','7','8','9','10','11','12'],
+      'dados' : [11,65,23,45,33,4,23,45,67,34,55,99]
+    }
+
+    vim.testeMesAnterior = {
+      'mes' : 'Janeiro',
+      'dias' : ['1','2','3','4','5','6','7','8','9','10','11','12'],
+      'dados' : [11,65,23,45,33,4,23,45,67,34,55,99]
     }
 
 
+    vim.testeComparativo = {
+      'mes' : ['2016', '2017'],
+      'dias' : ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+      'dados' : [[65, 59, 80, 81, 56, 55, 40],[28, 48, 40, 19, 86, 27, 90]]
+    }
+
+
+    vim.getMesAtual = function(){
+        return vim.testeMesAtual;
+    }
+
+    vim.getMesAnterior = function(){
+        return vim.testeMesAnterior;
+    }
+
+    vim.getComparativo = function(){
+        return vim.testeComparativo;
+    }
+
 
  }
- 
