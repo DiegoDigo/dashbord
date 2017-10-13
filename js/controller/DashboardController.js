5483050fa5527a827de257b5a3ee83cd6d678549
Diego <di3g0d0ming05@gmail.com>
2017-10-13 17:11:21 -0300

    alteraçao layout

--- js/controller/DashboardController.js
+++ js/controller/DashboardController.js
@@ -7,18 +7,20 @@ DashboardController.$inject = ['DashboardService', '$scope'];
 function DashboardController(DashboardService,$scope) {
 
     var vim = this;
-
+    vim.graf_anual_ano = undefined;
     vim.gastos = DashboardService.getGastos();
     vim.comandas = DashboardService.getComandas();
     vim.prds = DashboardService.getPrds();
-
+    vim.anos = [2017,2018,2019];
 
     $scope.colors = ['#45b7cd'];
     $scope.labels = vim.gastos.dias;
     $scope.series = vim.gastos.mes;
     $scope.data = vim.gastos.dados;
 
-    
+    vim.ano_atual = new Date();
+    vim.ano = vim.ano_atual.getFullYear();
+
     $scope.colors_comanda = ['#45b7cd', '#A5D6A7', '#DCDCDC'];
     $scope.labels_comanda = vim.comandas.dias;    
     $scope.data_comanda = vim.comandas.dados;
@@ -34,6 +36,11 @@ function DashboardController(DashboardService,$scope) {
 
     vim.verComandas = function (points, evt) {
         alert(points[0]._model.label);
+        
+    }
+
+    vim.validar = function(){
+        alert(vim.graf_anual_ano, vim.ano);
         
     }
 
